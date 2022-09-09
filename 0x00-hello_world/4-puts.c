1a446eb511baad722d0715fe2712a15fcca6381a
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-08 17:04:47 -0700

    Correction made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ *  * main -Main block
  *
  *   *
  *
  *    * Return: Always 0 (Success)
  *
- *     */
+ *     
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
