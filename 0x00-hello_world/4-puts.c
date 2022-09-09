bc83d0371a200f77ce5dcc741b587016ab8c738a
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-08 17:01:25 -0700

    Betty Errors corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,4 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
 /**
  *
  *  * main - Entry point
@@ -12,13 +8,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
