ae25f46ec8ba64e723fb2a20d86bc70b7f1c2792
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-13 14:59:49 -0700

    Program added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Create a function that prints the alphabet, in lowercase
+ *
+ *
+ *
+ * Return: Always 0 (Succes)
+ *
+ */
+
+void print_alphabet(void)
+
+{
+
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+}
