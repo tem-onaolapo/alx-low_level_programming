7731905276e61c2573ded92d6112e84b4e51c7ea
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-13 15:10:28 -0700

    Program added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main."
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	int n, count;
+	co = 0;
+	while (co < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+
+		{
+			_putchar(n);
+		}
+
+		count++;
+
+		_putchar('\n');
+	}
+
+}
