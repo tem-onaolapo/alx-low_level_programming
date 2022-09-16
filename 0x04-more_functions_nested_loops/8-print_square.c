514cf3173ce71c243af40e072035b7a747676be3
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-16 12:50:37 -0700

    corrected

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,7 +16,10 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		_putchar('#');
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
