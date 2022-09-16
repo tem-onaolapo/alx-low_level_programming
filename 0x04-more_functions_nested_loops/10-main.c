4be65e09d253fb8cea821afeec697adf66bc4ccf
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-16 14:55:53 -0700

    Program added

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++;)
+		{
+			if (j <= (size -  i))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
