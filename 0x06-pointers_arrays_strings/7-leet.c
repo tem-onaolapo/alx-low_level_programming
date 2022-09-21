f94f5f341ab52d6bdb5522883a2d34f8a281a4fd
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-21 14:05:18 -0700

    Corrected

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 char *leet(char *n)
 {
 	int i, x;
-	int find[] = {'a', 'A', 'e', 'E', 'o'', 'O', 't', 'T', 'l', 'L'};
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 
 	int replacer[] = {'4', '3', '0', '7', '1'};
 
