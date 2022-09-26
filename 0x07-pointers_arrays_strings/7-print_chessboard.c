4dd766db4232430b7444a48a45a286cd941ac5e2
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-26 15:02:17 -0700

    Correted

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,10 @@ void print_chessboard(char (*a)[8])
 {
 	int i, n;
 
-	for (i = 0; i < 8; n++)
-		_putchar(a[i][n]);
-	_putchar('\n');
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
 }
