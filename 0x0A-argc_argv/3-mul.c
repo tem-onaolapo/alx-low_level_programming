794875aa01fa14f49e7c9d181fabb2074202bec7
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-30 20:29:26 -0700

    Neew program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Program that takes firsst two integer arguments and prints the product
+ * @argc: Number of command line argumets
+ * @argv: Array name
+ */
+
+int main (int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	else
+	{
+		j = 1;
+
+		for (i = 1; i < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
