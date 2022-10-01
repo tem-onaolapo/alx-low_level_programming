2238bd6d2b218f3bfec09f0918a2928df7104c5c
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-30 20:56:55 -0700

    program added

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,4 +11,34 @@
  * Return: Always 0 (Success)
  */
 
+int main(int argc, char *argv[])
+{
+	int i, j, length, sum;
+	char *ptr;
 
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for ( j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					print("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
