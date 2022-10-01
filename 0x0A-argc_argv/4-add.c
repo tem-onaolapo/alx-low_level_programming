e23b05b8a3ec31dc8fb584520fd1f8186252dd1d
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-09-30 20:47:39 -0700

    new program

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: Always 0 (Success)
+ */
+
+
