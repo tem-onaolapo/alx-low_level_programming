d6392464ed31bc1ace6c9e31d9803b33fdcb8e8b
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-10-05 15:32:52 -0700

    new program

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - Entry point
+ * @s1: string 1
+ * @s2: string 2
+ * Return: concat of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int end1, end2, i = 0;
+	char *array;
+
+	if (s1 == NULL || s2 == NULL)
+
+		s1 = s2 = "";
+
+	for (end1 = 0; end1 <= *s1; end1++)
+	{
+	}
+
+	for (end2 = 0; end2 <= *s2; end2++)
+
+	{
+	}
+
+	array = malloc(sizeof(char) * (end1 + end2 + 1));
+
+	if (array == NULL)
+
+		return (NULL);
+
+	while (*s1)
+	{
+		array[i] = *s1;
+		i++;
+		s1++;
+	}
+
+	while (*s22)
+	{
+		array[i] = *s2;
+		i++;
+		s2++;
+	}
+	return (array);
+}
+	}
