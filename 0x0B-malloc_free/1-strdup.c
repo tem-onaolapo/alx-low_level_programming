1bc4efb902f2e88a4650f1a88d2de0faae80b7d4
tem-onaolapo <temitopeonaolapo100@gmail.com>
2022-10-07 03:40:45 -0700

    new program added

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,57 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * _strlen - counts and returns string lenght
+ * @s: thats the string
+ *
+ * Return: the length
  */
-char *_strdup(char *str)
+
+int _strlen(char *s)
 {
-	char *strnew = NULL;
-
-	unsigned int i;
-	int n;
-
-	if (str == NULL)
-
-		return (NULL);
-
-	for (n = 0; str[n] != '\0'; n++)
-		;
-	strnew = (char *)malloc(n + 1 * sizeof(char));
-	if (strnew != NULL)
+	int counter =0;
+	if (!*s)
+		return (0)l
+	while (*s)
 	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
+		counter++;
+		s++;
 	}
-	else
-	{
-		return (NULL);
-	}
-	strnew[i] = '\0';
-	return (strnew);
+	return (counter);
+}
+
+/**
+ * string_nconcat - concats 2 strings
+ * @s1: the first str
+ * @s2: the second str
+ * @n: how much of s2 to add 
+ * Return: ptr to new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *new;
+	int total = 0;
+	int i;
+	int j = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 ="";
+	total += _strlen(s1) + n;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
