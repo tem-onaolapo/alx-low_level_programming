24cb7d13e6e6d0610109c303ea06814c91bce65a
Tem Onaolapo <temitopeonaolapo100@gmail.com>
2025-07-08 23:55:13 +0000

    Fixing bugs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -11,12 +12,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if(int n > 0){
+	if(n > 0){
 		printf("%d is positive\n", n);
-	} elseif(int n == 0){	
-		printf("%d is zero\n", n);
-	} else {	
-		printf("%d is negatvie\n", n);
 	}
+	
+	else if(n == 0){	
+		printf("%d is zero\n", n);
+	}
+	
+	else {	
+		printf("%d is negative\n", n);
+	}
+	
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+##Learning the basic of C programming language <br>
+This directory contains tasks used to learn if-else statements in C
