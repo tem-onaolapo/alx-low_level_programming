739b3a3612cc0818087797bb3d42f8d263af443d
Tem Onaolapo <temitopeonaolapo100@gmail.com>
2025-07-09 10:18:33 +0000

    for loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
