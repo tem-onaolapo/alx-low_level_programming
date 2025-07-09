e6d07ad360179a5204fb57987240904b3548dc18
Tem Onaolapo <temitopeonaolapo100@gmail.com>
2025-07-09 09:42:27 +0000

    printing alphabets with while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while(letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
